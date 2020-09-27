fea2ed4c17867c5006f431462a1bad7287cf9645
Marwa7246 <marwa.ragheb@gmail.com>
2020-09-26 23:44:34 -0400

    Implement mapdReducer and useApplicationMapData

---
+++ client/src/hooks/useApplicationMapData.js
@@ -0,0 +1,28 @@
+import { useEffect, useReducer } from 'react';
+import axios from 'axios';
+import mapReducer, { SET_MAP_DATA } from '../reducers/mapReducer';
+
+const useApplicationMapData = () => {
+  const [stateMap, dispatch] = useReducer(mapReducer, {
+    mapData: [],
+    userFavourite: [],
+    loading: true,
+  });
+
+  useEffect(() => {
+    axios({
+      method: 'GET',
+      url: 'https://disease.sh/v3/covid-19/countries',
+    }).then(({ data }) => {
+      console.group(data)
+      // update the state with the result
+      dispatch({ type: SET_MAP_DATA, mapData: data });
+    });
+  }, []);
+
+  return {
+    stateMap,
+  };
+};
+
+export default useApplicationMapData;
