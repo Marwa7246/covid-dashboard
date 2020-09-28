e423c35c72ce0bff7164f1da73703ab7169d8e6e
Marwa7246 <marwa.ragheb@gmail.com>
2020-09-27 20:07:57 -0400

    Change sidebar pictures

--- client/src/layouts/Admin.js
+++ client/src/layouts/Admin.js
@@ -15,8 +15,8 @@ import routes from "routes.js";
 
 import styles from "assets/jss/material-dashboard-react/layouts/adminStyle.js";
 
-import bgImage from "assets/img/sidebar-2.jpg";
-import logo from "assets/img/reactlogo.png";
+import bgImage from "assets/img/north_america.png";
+import logo from "assets/img/covid.png";
 
 let ps;
 
