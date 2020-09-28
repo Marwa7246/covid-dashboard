53644cb4e52c8676248c0de94bb6c36a1a34c23a
Marwa7246 <marwa.ragheb@gmail.com>
2020-09-28 14:47:17 -0400

    Combine axios calls in one call

--- client/src/hooks/useApplicationData.js
+++ client/src/hooks/useApplicationData.js
@@ -2,10 +2,12 @@ import { useEffect, useReducer } from "react";
 import axios from "axios";
 import dataReducer, {
   SET_USERS,
-  SET_HISTORICAL_GLOBAL,
-  SET_YESTERDAY_CONTINENTS,
-  SET_YESTERDAY_GLOBAL,
-  SET_WORLD_COVID_NEWS,
+  // SET_HISTORICAL_GLOBAL,
+  // SET_YESTERDAY_CONTINENTS,
+  // SET_YESTERDAY_GLOBAL,
+  // SET_MAP_DATA,
+  // SET_WORLD_COVID_NEWS,
+  SET_APPLICATION_DATA,
 } from "../reducers/dataReducer";
 
 // const API_KEY = process.env.NEWS_API_KEY;
@@ -18,69 +20,101 @@ const useApplicationData = () => {
     globalHistorical: {},
     yesterdayContinents: [],
     yesterdayGlobal: {},
+    mapData: [],
     worldCovidNews: {},
     loading: true,
   });
 
-  useEffect(() => {
-    axios({
-      method: "GET",
-      url: "/api/users",
-    }).then(({ data }) => {
-      // update the state with the result
-      dispatch({ type: SET_USERS, users: data });
-    });
-  }, []);
+  const newsUrl = `http://newsapi.org/v2/top-headlines?apiKey=${process.env.REACT_APP_NEWS_API_KEY}&language=en&q=covid&sortby=publishedAt`
+  
+  console.log(newsUrl)
 
   useEffect(() => {
-    axios({
-      method: "GET",
-      url: "https://disease.sh/v3/covid-19/historical/all?lastdays=20",
-    }).then(({ data }) => {
+    Promise.all([
+    axios.get(" https://disease.sh/v3/covid-19/continents"),
+    axios.get(" https://disease.sh/v3/covid-19/historical/all?lastdays=20"),
+    axios.get(" https://disease.sh/v3/covid-19/all"),
+    axios.get(" https://disease.sh/v3/covid-19/countries"),
+    axios.get(newsUrl)
+  ]).then((all) => {
+    console.log(all)
       // update the state with the result
-      dispatch({ type: SET_HISTORICAL_GLOBAL, globalHistorical: data });
+      dispatch({ 
+        type: SET_APPLICATION_DATA, 
+        yesterdayContinents: all[0].data,
+        globalHistorical: all[1].data,
+        yesterdayGlobal: all[2].data,
+        mapData: all[3].data,
+        worldCovidNews: all[4].data, 
+      });
+
       // console.log(data);
     });
   }, []);
 
-  useEffect(() => {
-    axios({
-      method: "GET",
-      url: " https://disease.sh/v3/covid-19/continents",
-    }).then(({ data }) => {
-      // update the state with the result
-      dispatch({ type: SET_YESTERDAY_CONTINENTS, yesterdayContinents: data });
-      // console.log(data);
-    });
-  }, []);
+  console.log(state)
+{
 
-  useEffect(() => {
-    axios({
-      method: "GET",
-      url: " https://disease.sh/v3/covid-19/all",
-    }).then(({ data }) => {
-      // update the state with the result
-      dispatch({ type: SET_YESTERDAY_GLOBAL, yesterdayGlobal: data });
-      // console.log(data);
-    });
-  }, []);
 
-  useEffect(() => {
-    // ****** Place API key in the environment variables ****
-    axios({
-      method: "GET",
-      url:
-        // NEWS_URL,
-        "http://newsapi.org/v2/top-headlines?apiKey=93894a50640e4b77a88f4d1b8c720c4d&language=en&q=covid&sortby=publishedAt",
-    }).then(({ data }) => {
-      // update the state with the result
-      dispatch({ type: SET_WORLD_COVID_NEWS, worldCovidNews: data });
-      // console.log(
-      //  "useApplicationData.js @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-      // console.log(data);
-    });
-  }, []);
+  // useEffect(() => {
+  //   axios({
+  //     method: "GET",
+  //     url: "/api/users",
+  //   }).then(({ data }) => {
+  //     // update the state with the result
+  //     dispatch({ type: SET_USERS, users: data });
+  //   });
+  // }, []);
 
+  // useEffect(() => {
+  //   axios({
+  //     method: "GET",
+  //     url: "https://disease.sh/v3/covid-19/historical/all?lastdays=20",
+  //   }).then(({ data }) => {
+  //     // update the state with the result
+  //     dispatch({ type: SET_HISTORICAL_GLOBAL, globalHistorical: data });
+  //     // console.log(data);
+  //   });
+  // }, []);
+
+  // useEffect(() => {
+  //   axios({
+  //     method: "GET",
+  //     url: " https://disease.sh/v3/covid-19/continents",
+  //   }).then(({ data }) => {
+  //     // update the state with the result
+  //     dispatch({ type: SET_YESTERDAY_CONTINENTS, yesterdayContinents: data });
+  //     // console.log(data);
+  //   });
+  // }, []);
+
+  // useEffect(() => {
+  //   axios({
+  //     method: "GET",
+  //     url: " https://disease.sh/v3/covid-19/all",
+  //   }).then(({ data }) => {
+  //     // update the state with the result
+  //     dispatch({ type: SET_YESTERDAY_GLOBAL, yesterdayGlobal: data });
+  //     // console.log(data);
+  //   });
+  // }, []);
+
+  // useEffect(() => {
+  //   // ****** Place API key in the environment variables ****
+  //   axios({
+  //     method: "GET",
+  //     url:
+  //       // NEWS_URL,
+  //       "###",
+  //   }).then(({ data }) => {
+  //     // update the state with the result
+  //     dispatch({ type: SET_WORLD_COVID_NEWS, worldCovidNews: data });
+  //     // console.log(
+  //     //  "useApplicationData.js @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+  //     // console.log(data);
+  //   });
+  // }, []);
+}
   return {
     state,
     dispatch,
--- client/src/reducers/dataReducer.js
+++ client/src/reducers/dataReducer.js
@@ -1,8 +1,10 @@
 export const SET_USERS = "SET_USERS";
-export const SET_HISTORICAL_GLOBAL = "SET_HISTORICAL_GLOBAL";
-export const SET_YESTERDAY_CONTINENTS = "SET_YESTERDAY_CONTINENTS";
-export const SET_YESTERDAY_GLOBAL = "SET_YESTERDAY_GLOBAL";
-export const SET_WORLD_COVID_NEWS = "SET_WORLD_COVID_NEWS";
+// export const SET_HISTORICAL_GLOBAL = "SET_HISTORICAL_GLOBAL";
+// export const SET_YESTERDAY_CONTINENTS = "SET_YESTERDAY_CONTINENTS";
+// export const SET_YESTERDAY_GLOBAL = "SET_YESTERDAY_GLOBAL";
+// export const SET_WORLD_COVID_NEWS = "SET_WORLD_COVID_NEWS";
+// export const SET_MAP_DATA ='SET_MAP_DATA';
+export const SET_APPLICATION_DATA = 'SET_APPLICATION_DATA'
 
 const dataReducer = (state, action) => {
   // const actions = {
@@ -21,37 +23,67 @@ const dataReducer = (state, action) => {
         loading: false,
       };
     }
-    case SET_HISTORICAL_GLOBAL: {
+
+    case SET_APPLICATION_DATA: {
       return {
         ...state,
         globalHistorical: action.globalHistorical,
-        loading: false,
-      };
-    }
-
-    case SET_YESTERDAY_CONTINENTS: {
-      return {
-        ...state,
         yesterdayContinents: action.yesterdayContinents,
-        loading: false,
-      };
-    }
-
-    case SET_YESTERDAY_GLOBAL: {
-      return {
-        ...state,
         yesterdayGlobal: action.yesterdayGlobal,
+        mapData: action.mapData,
+        worldCovidNews: action.worldCovidNews,
+
         loading: false,
       };
     }
 
-    case SET_WORLD_COVID_NEWS: {
-      return {
-        ...state,
-        worldCovidNews: action.worldCovidNews,
-        loading: false,
-      };
-    }
+    // case SET_HISTORICAL_GLOBAL: {
+    //   return {
+    //     ...state,
+    //     globalHistorical: action.globalHistorical,
+    //     loading: false,
+    //   };
+    // }
+
+    // case SET_YESTERDAY_CONTINENTS: {
+    //   return {
+    //     ...state,
+    //     yesterdayContinents: action.yesterdayContinents,
+    //     loading: false,
+    //   };
+    // }
+
+    // case SET_YESTERDAY_GLOBAL: {
+    //   return {
+    //     ...state,
+    //     yesterdayGlobal: action.yesterdayGlobal,
+    //     loading: false,
+    //   };
+    // }
+
+    // case SET_MAP_DATA: {
+    //   return {
+    //     ...state,
+    //     mapData: action.mapData,
+    //     loading: false,
+    //   };
+    // }
+
+    // case SET_USER_FAVOURITES: {
+    //   return {
+    //     ...state,
+    //     userFavourites: action.userFavourites,
+    //     loading: false,
+    //   };
+    // }
+
+    // case SET_WORLD_COVID_NEWS: {
+    //   return {
+    //     ...state,
+    //     worldCovidNews: action.worldCovidNews,
+    //     loading: false,
+    //   };
+    // }
 
     default:
       throw new Error(
