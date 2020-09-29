8452ad2a8fcd124289c3f263cb3dbafd7a61025a
Marwa7246 <marwa.ragheb@gmail.com>
2020-09-29 14:30:00 -0400

    Add map infowindow styles

--- client/src/components/Maps/InforWindowName.js
+++ client/src/components/Maps/InforWindowName.js
@@ -11,16 +11,16 @@ function InfoWindowName(props) {
 
   return (
     <div className='InfoWindow' >
-      <img className='image'
+      <img className='InfoWindow-image'
               src= {image}
               alt={country}
               onClick={onClick}
             />
-      <span><strong>{country}  </strong> </span>
+      <span><strong> {country}  </strong> </span>
     <div><strong>Cases:</strong>  {cases} </div>
-    <div><strong> Deaths:</strong>{deaths} </div>
-    <div><strong> Recovered:</strong>{recovered} </div>
-    <div><strong> Last Updated:</strong>{updated} </div>
+    <div><strong>Deaths:</strong>{deaths} </div>
+    <div><strong>Recovered:</strong>{recovered} </div>
+    <div><strong>Last Updated:</strong>{updated} </div>
     <div ><i className="fa fa-bell fa-spin fa-2x "></i></div>
   </div>
   );
