9eb766c8093a2aa248884bec813ed5a9810f1777
Sameer Nandan Menda <msnandan.20@gmail.com>
2020-09-29 01:13:46 -0400

    Added logic to save state

--- client/src/views/Login.js
+++ client/src/views/Login.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 // core components
 import GridItem from "components/Grid/GridItem.js";
@@ -11,6 +11,28 @@ import CardBody from "components/Card/CardBody.js";
 import CardFooter from "components/Card/CardFooter.js";
 
 export default function Login() {
+  const [inputs, setInputs] = useState({
+    username: "",
+    password: "",
+  });
+
+  const [submitted, setSubmitted] = useState(false);
+  const { username, password } = inputs;
+
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setInputs((inputs) => ({ ...inputs, [name]: value }));
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    setSubmitted(true);
+    if (username && password) {
+      // Add code to return navigate to the home page
+    }
+  }
+
   return (
     <div>
       <GridContainer>
@@ -24,11 +46,16 @@ export default function Login() {
                 <GridItem xs={12} sm={12} md={5}>
                   <CustomInput
                     labelText="Email address"
-                    id="email-address"
+                    id="email"
+                    value={username}
+                    onChange={handleChange}
                     formControlProps={{
                       fullWidth: true,
                     }}
                   />
+                  {submitted && !username && (
+                    <div className="invalid-feedback">Email ID is required</div>
+                  )}
                 </GridItem>
               </GridContainer>
               <GridContainer>
@@ -36,15 +63,28 @@ export default function Login() {
                   <CustomInput
                     labelText="Password"
                     id="password"
+                    value={password}
+                    onChange={handleChange}
                     formControlProps={{
                       fullWidth: true,
                     }}
                   />
+                  {submitted && !password && (
+                    <div className="invalid-feedback">Password is required</div>
+                  )}
                 </GridItem>
               </GridContainer>
             </CardBody>
             <CardFooter>
-              <Button color="primary">Login</Button>
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={() => {
+                  alert("clicked");
+                }}
+              >
+                Login
+              </Button>
             </CardFooter>
           </Card>
         </GridItem>
