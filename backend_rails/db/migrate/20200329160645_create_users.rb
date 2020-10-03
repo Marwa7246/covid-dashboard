f7e9894786dfaafd6f29ec9573cdaf3122535976
Sameer Nandan Menda <msnandan.20@gmail.com>
2020-10-03 14:14:28 -0400

    Added unique constraint to user email

--- backend_rails/db/migrate/20200329160645_create_users.rb
+++ backend_rails/db/migrate/20200329160645_create_users.rb
@@ -3,8 +3,9 @@ class CreateUsers < ActiveRecord::Migration[6.0]
     create_table :users do |t|
       t.string :first_name
       t.string :last_name
-      t.string :email
+      t.string :email, unique: true
       t.string :password
+      t.string :mobile
 
       t.timestamps
     end
--- backend_rails/db/schema.rb
+++ backend_rails/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_30_061945) do
+ActiveRecord::Schema.define(version: 2020_10_03_174433) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2020_09_30_061945) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "mobile"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "favourites", "users"
