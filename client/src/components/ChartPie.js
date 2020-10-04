1af86e3d5ea9e63e901b248961290c696436b795
Sameer Nandan Menda <msnandan.20@gmail.com>
2020-10-04 16:25:32 -0400

    Updated view

--- client/src/components/ChartPie.js
+++ client/src/components/ChartPie.js
@@ -1,10 +1,7 @@
 import ChartistGraph from "react-chartist";
 import legend from "chartist-plugin-legend";
 
-
-
 import React from "react";
-
 
 import { makeStyles } from "@material-ui/core/styles";
 
@@ -19,69 +16,64 @@ import CardFooter from "components/Card/CardFooter.js";
 
 import styles from "assets/jss/material-dashboard-react/views/dashboardStyle.js";
 
-
-
-
 const useStyles = makeStyles(styles);
 
-
-export default function ChartPie({state}) {
-
+export default function ChartPie({ state }) {
   //  console.log(state)
 
-  const yesterdayContinents =state.yesterdayContinents 
+  const yesterdayContinents = state.yesterdayContinents;
 
-  let seriesNormalized = []
-  let labels = []
+  let seriesNormalized = [];
+  let labels = [];
 
-    if (yesterdayContinents.length > 0) {
-      labels = yesterdayContinents.map(cont => cont.continent)
-      const series = yesterdayContinents.map(cont => cont.cases)
-      const totalCases = series.reduce((a,c) => a+c)
-      seriesNormalized = series.map(ele => Math.floor(ele * 100 /totalCases))
-     }
+  if (yesterdayContinents.length > 0) {
+    labels = yesterdayContinents.map((cont) => cont.continent);
+    const series = yesterdayContinents.map((cont) => cont.cases);
+    const totalCases = series.reduce((a, c) => a + c);
+    seriesNormalized = series.map((ele) =>
+      Math.floor((ele * 100) / totalCases)
+    );
+  }
 
+  const dataPie = {
+    labels: seriesNormalized,
+    series: seriesNormalized,
+  };
 
+  const legendPie = {
+    names: labels,
+    types: ["#c40000", "#e75480", "#ecb753", "#f7944a", "#4f5355", "#009000"],
+  };
 
-    const dataPie = {
-      labels: seriesNormalized,
-      series: seriesNormalized
-    };
+  function createLegend(json) {
+    const legend = [];
 
-    const legendPie = {
-      names: labels,
-      types: ["#c40000", "#e75480", "#ecb753", "#f7944a", "#4f5355", "#009000"]
-    };
-
-    function createLegend (json) {
-      const legend = [];
-
-      for (let i = 0; i < json["names"].length; i++) {
-        const type = "fa fa-circle text-" + json["types"][i];
-        const textColor = { "color": json["types"][i]}
-        legend.push(<span key={i} ><i className={type} style={textColor} /> {json["names"][i] }</span>);
-        legend.push(" ");
-
-      }
-      return legend;
+    for (let i = 0; i < json["names"].length; i++) {
+      const type = "fa fa-circle text-" + json["types"][i];
+      const textColor = { color: json["types"][i] };
+      legend.push(
+        <span key={i}>
+          <i className={type} style={textColor} /> {json["names"][i]}
+        </span>
+      );
+      legend.push(" ");
     }
+    return legend;
+  }
 
+  const options = {
+    showLabel: true,
+    // labelInterpolationFnc: function(value) {
+    //   return value[0]
+    // }
+  };
 
-    const options = {
-      showLabel: true,  
-      // labelInterpolationFnc: function(value) {
-      //   return value[0]
-      // }
-    };
-
-
-const classes = useStyles();
-
+  const classes = useStyles();
 
   return (
     <div>
       <Card chart>
-        <CardHeader color="info"> 
+        <CardHeader color="info">
           <ChartistGraph data={dataPie} options={options} type="Pie" />
         </CardHeader>
         <CardBody>
@@ -89,14 +81,11 @@ const classes = useStyles();
           <p className={classes.legend}>{createLegend(legendPie)}</p>
         </CardBody>
         <CardFooter chart>
-          <div className={classes.stats}>
+          {/* <div className={classes.stats}>
             <AccessTime /> campaign sent 2 days ago
-          </div>
+          </div> */}
         </CardFooter>
       </Card>
-
     </div>
-  )
-
-
+  );
 }
