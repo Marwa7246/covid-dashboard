6c2705e256f8b58934d6220c72005085516e1791
Marwa7246 <marwa.ragheb@gmail.com>
2020-11-25 13:17:30 -0500

    Add news route

--- backend_rails/app/controllers/api/news_controller.rb
+++ backend_rails/app/controllers/api/news_controller.rb
@@ -1,2 +1,8 @@
 class Api::NewsController < ApplicationController
+
+  skip_before_action :require_login
+  def index
+    data = Datum.all
+    render json: data
+  end
 end
--- backend_rails/config/routes.rb
+++ backend_rails/config/routes.rb
@@ -2,18 +2,21 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   namespace :api do
-    resource :users
-    resources :data
-    resources :favourites
-    resources :twilios
+    resource :users, only: [:create, :show] do
+    resources :favourites, only: [:create, :destroy] 
+    end
+
+    # resources :data
+    resources :twilios , only: [:create]
 
     get '/users/:email', to: 'users#show'
     get '/users', to: 'users#index'
+    delete '/users/favourites/:email', to: 'favourites#destroy'
 
   end
 
 
-  get 'api/news' => 'news#index'
+  get '/api/news' => 'api/news#index'
   
 
   post "/login", to: "auth#login"
