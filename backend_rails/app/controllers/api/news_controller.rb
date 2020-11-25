2f99194427dd64b1f10345141f3483994b262fc3
Marwa7246 <marwa.ragheb@gmail.com>
2020-11-25 15:02:29 -0500

    Add excon

--- backend_rails/app/controllers/api/news_controller.rb
+++ backend_rails/app/controllers/api/news_controller.rb
@@ -1,8 +1,26 @@
 class Api::NewsController < ApplicationController
 
   skip_before_action :require_login
+  
+
+
   def index
-    data = Datum.all
-    render json: data
+    info = find_country()
+    render json: info
+  end
+
+  private
+  def request_api(url)
+    response = Excon.get(
+      url,
+
+    )
+    return nil if response.status != 200
+    JSON.parse(response.body)
+  end
+  def find_country()
+    request_api(
+      "https://disease.sh/v3/covid-19/historical?lastdays=30"
+    )
   end
 end
